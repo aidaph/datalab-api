e2640dc4a4506549ce1ebb785aaa1031ef60c2bb
Aida Palacio <aidaph@ifca.unican.es>
2023-06-15 11:07:35 +0200

    Fix some functionality in deployments api

--- api/routers/deployments.py
+++ api/routers/deployments.py
@@ -8,16 +8,16 @@ from app.users import (
 #    github_oauth_client
 )
 from enum import Enum
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from kubernetes import client, config
 from kubernetes.client.models.v1_namespace import V1Namespace
 from kubernetes.client.rest import ApiException
 
 from sqlalchemy.orm import Session
+from typing import Annotated
 
 import httpx
 import logging, pathlib
-import os
 import requests
 import yaml
 
@@ -25,9 +25,9 @@ import yaml
 #deployment.Base.metadata.create_all(bind=engine)
 
 class DeploymentType(str, Enum):
-    ids = "IDS"
-    climate = "Climate"
-    master = "DataScienceHub"
+    ids = "ids"
+    climate = "ipcc"
+    master = "dataScienceHub"
     dummy = "dummy"
     ## Add more
     face = "FACE"
@@ -39,17 +39,18 @@ router = APIRouter(
 log = logging.getLogger(__name__)
 
 def get_kubecoreapi():
-    config.load_kube_config(config_file=os.environ['KUBECONFIG'])
+    config.load_kube_config()
     return client.CoreV1Api()
 
 def get_kubeappsapi():
-    config.load_kube_config(config_file=os.environ['KUBECONFIG'])
+    config.load_kube_config()
     return client.AppsV1Api()
 
 k8s_apps_v1 = get_kubeappsapi()
 k8s_core_v1 = get_kubecoreapi()
 
 def create_kube_namespace(name: str):
+
     v1 = get_kubecoreapi()
     exception = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
         detail="Namespace already exists", 
@@ -58,7 +59,7 @@ def create_kube_namespace(name: str):
     ### TODO: Create it only if is an acceptable name
     for nameSpace in nameSpaceList.items:
         if nameSpace.metadata.name == "jupyterhub-"+name:
-            print("Namespace already exists")
+            raise exception
             return nameSpace.metadata.name
    
     body = client.V1Namespace(
@@ -70,12 +71,45 @@ def create_kube_namespace(name: str):
         print("Exception when calling CoreV1Api->create_namespace: %s\n" % e)
 
 
+@router.get("/types")
+def get_deployment_types():
+    types = [t for t in DeploymentType]
+    return types
+
+
+@router.get("/running")
+def get_running_jupyterhubs():
+    """
+    Get current Jupyterhub environments running
+    """
+    v1 = get_kubecoreapi()
+    exception = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+        detail="Namespace already exists", 
+        headers={"WWW-Authenticate": "Bearer"})
+    nameSpaceList = v1.list_namespace()
+    ### TODO: Create it only if is an acceptable name
+    deploys = []
+    for name in nameSpaceList.items:
+        if "jupyterhub-" in name.metadata.name:
+            n = name.metadata.name.replace("jupyterhub-", "")
+            deploys.append(n)
+    return deploys 
+
+
 @router.post("/{namespace}/jupyterhub")
-def create_jupyterhub_environment(namespace: str = DeploymentType.dummy):
+def create_jupyterhub_environment(namespace: str  = DeploymentType.dummy):
     """
     Create new jupyterhub environment inside the namespace = {server_name}
+
+    Valid types = dummy, ids, ipcc, datasciencehub, face
     """
-    namespace_name = create_kube_namespace(namespace) # Check if exists
+    exception = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+        detail="Deployment name is not valid, please choose a valid type")
+    # Verify that namespace name is a valid type of the deployment type list
+    if namespace in get_deployment_types():
+        namespace_name = create_kube_namespace(namespace) # Check if exists
+    else:
+        raise exception
 
     # Check if the services are already created
     try:
@@ -93,7 +127,7 @@ def create_jupyterhub_environment(namespace: str = DeploymentType.dummy):
     create_rbac(namespace_name)
     create_configmap(namespace_name)
     create_pvc(namespace_name)
-    create_ingress(namespace_name)
+    create_ingress(namespace)
 
     try:
         get_current_kubehubdeployments(namespace=namespace_name)
@@ -101,17 +135,27 @@ def create_jupyterhub_environment(namespace: str = DeploymentType.dummy):
     except HTTPException:
         print("Hub exists in the namespace")
 
-    # if namespace = DeploymentType.IDS:
-    #     if check_kafka():
-    #         print("Kafka already exists")
-    #     else:
-    #         create_kafka(namespace)
-
+    url = f"https://{namespace}.datalab.ifca.es"
+    return {"datalab-url: ": f"{url}"}
 
 @router.delete("/{namespace}/jupyterhub")
 def delete_jupyterhub_namespace(namespace: str = DeploymentType.dummy):
-    return k8s_core_v1.delete_namespace(name=namespace)
+    return k8s_core_v1.delete_namespace(name="jupyterhub-"+namespace)
 
+
+@router.get("/{namespace}/jupyterhub")
+def get_url_jupyterhub_namespace(namespace: str = DeploymentType.dummy):
+    """
+    Get info of the jupyterhub environment
+
+    Valid types = dummy, ids, ipcc, datasciencehub, face
+    """
+    if namespace in get_deployment_types():
+        return {f"Jupyterhub for {namespace} is running at https://{namespace}.datalab.ifca.es"}
+    else:
+        exception = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+        detail="Deployment name is not valid or does not exist")
+        raise exception
 
 def create_services(namespace = DeploymentType.dummy):
     
@@ -200,12 +244,6 @@ def create_rbac(namespace:str):
     resp = api_instance.create_namespaced_role_binding(namespace=namespace,
                                                        body=rolebinding)
     
-def create_ingress(namespace: str):
-    networking_v1_api = client.NetworkingV1Api()
-    with open("yamls/ingress.yaml") as f:
-        dep = yaml.safe_load(f)
-        resp = networking_v1_api.create_namespaced_ingress(namespace=namespace,
-                                                               body=dep)
 
 def create_proxydeployments(namespace = str):
     ## Create the proxy deployment
@@ -225,6 +263,7 @@ def create_hubdeployments(namespace = str):
         print("Deployment Hub created. status='%s'" % resp)
 
     ## End: Once the jupyterhub is created the user can create new server
+
 
 @router.post("/{namespace}/kafka")
 def create_kafka(namespace = str):
@@ -257,7 +296,7 @@ def create_kafka(namespace = str):
 def create_thredds(namespace = str):
     pass
 
-@router.get("/{namespace}/services")
+#@router.get("/{namespace}/services")
 def get_current_kubeservices(namespace: str):
     print("list services from namespace: ", namespace)
     ret = k8s_core_v1.list_namespaced_service(namespace=namespace)
@@ -284,7 +323,7 @@ def get_current_kubeproxydeployments(namespace: str):
             continue
     return("No proxy in the namespace")
     
-@router.get("/{namespace}/deployments")   
+#@router.get("/{namespace}/deployments")   
 def get_current_kubehubdeployments(namespace: str):
     print("list deployments from namespace: ", namespace)
     ret = k8s_apps_v1.list_namespaced_deployment(namespace=namespace)
@@ -296,41 +335,3 @@ def get_current_kubehubdeployments(namespace: str):
             raise exception
         else:
             return("No hub in the namespace")
-
-
-def main():
-    """Start server"""
-    user = "aidaph"
-    hub_url = "https://vm021.pub.cloud.ifca.es:8000/hub/api"
-
-    session = make_session()
-    server_url = start_jupyterhub_server(session)
-    print(server_url)
-
-if __name__ == "__main__":
-    main()
-
-# @router.post("/deployments", response_model=Deployment)
-# async def create_deployment(
-#     deployment: client.V1Deployment = Depends(create_deployment_object), 
-#     db: Session = Depends(get_db), 
-#     api: client.AppsV1Api = Depends(get_kubeapi)):
-#     # Create Deployment
-#     resp = api.create_namespaced_deployment(
-#         body=deployment, namespace="ids"
-#     )
-
-#     print("\n[INFO] deployment `ids-deployment` created.\n")
-#     print("%s\t%s\t\t\t%s\t%s" % ("NAMESPACE", "NAME", "REVISION", "IMAGE"))
-#     print(
-#         "%s\t\t%s\t%s\t\t%s\n"
-#         % (
-#             resp.metadata.namespace,
-#             resp.metadata.name,
-#             resp.metadata.generation,
-#             resp.spec.template.spec.containers[0].image,
-#         )
-#     )
-    
-
-
